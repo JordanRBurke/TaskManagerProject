879331339a06cd876472d260c8c9d15415ad3239
JordanRBurke <jordanrobertburke@gmail.com>
2018-05-16 14:01:34 -0400

    In TaskAdapter made AlertDialog method work for both editing and removing without having to have two AlertDialogs.

--- app/src/main/java/com/jordanburke/taskmanagerproject/TaskAdapter.java
+++ app/src/main/java/com/jordanburke/taskmanagerproject/TaskAdapter.java
@@ -1,11 +1,13 @@
 package com.jordanburke.taskmanagerproject;
 
 import android.app.AlertDialog;
+import android.app.FragmentManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.Fragment;
 import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -35,10 +37,14 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
     private TaskClickListener taskClickListener;
     private boolean singleClickSelected;
     private CreateTaskFragment createTaskFragment;
+    private EditTaskFragment editTaskFragment;
+    private FragmentManager fragmentManager;
+    private TaskDao taskDao;
 
 
-    public TaskAdapter(List<Tasks> tasksList, Context mainActivity) {
+    public TaskAdapter(List<Tasks> tasksList, Context mainContext) {
         this.tasksList = tasksList;
+        this.fragmentManager = fragmentManager;
     }
 
     @NonNull
@@ -87,6 +93,8 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
         public void onClick(View v) {
             Context context = v.getContext();
             singleClickSelected = true;
+            builder("Would you like to edit this task?");
+
 
 
 
@@ -104,7 +112,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
 
         // Alerts user based on message passed in
         private AlertDialog builder(String message) {
-            Context context = itemView.getContext();
+            final Context context = itemView.getContext();
             AlertDialog.Builder builder = new AlertDialog.Builder(context);
             builder.setMessage(message)
                     .setTitle("Task")
@@ -113,20 +121,24 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
                         public void onClick(DialogInterface dialog, int which) {
                             Context context1 = itemView.getContext();
                             if (singleClickSelected) {
+                                Toast.makeText(context1, "If activated", Toast.LENGTH_SHORT).show();
+//                                MainActivity mainActivity = (MainActivity)context1;
+//                                mainActivity.getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, editTaskFragment).commit();
+                                singleClickSelected = false;
 
 
-
-                            }
-                            Toast.makeText(context1, "dialogComplete", Toast.LENGTH_SHORT).show();
-//                            tasksList.remove(getLayoutPosition());
-                            if (tasksList == tasksList.get(0)) {
-                                Toast.makeText(context1, "Error", Toast.LENGTH_SHORT).show();
                             } else {
-                                tasksList.remove(getAdapterPosition());
-                                notifyItemRemoved(getAdapterPosition());
+                                Toast.makeText(context1, "dialogComplete", Toast.LENGTH_SHORT).show();
+//                            tasksList.remove(getLayoutPosition());
+                                if (tasksList == tasksList.get(0)) {
+                                    Toast.makeText(context1, "Error", Toast.LENGTH_SHORT).show();
+                                } else {
+//                                    taskDao.deleteTask(tasks);
+                                    tasksList.remove(getAdapterPosition());
+                                    notifyItemRemoved(getAdapterPosition());
+                                }
+
                             }
-
-
                         }
 
                     })
